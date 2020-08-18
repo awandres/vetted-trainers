ad29047bd75ed8012e2a3ed6579544c949079692
Alex Wandres <alex.r.wandres@gmail.com>
2020-08-18 17:47:01 -0400

    changed GA linked account

--- components/layout.js
+++ components/layout.js
@@ -14,16 +14,17 @@ export default function Layout({ children, home }) {
   return (
     <html lang="en" className={styles.container}>
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144123323-1"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169937814-1"></script>
 
       <script
         dangerouslySetInnerHTML={{
           __html: `
+
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-144123323-1');
+            gtag('config', 'UA-169937814-1');
             `,
         }}
       />
