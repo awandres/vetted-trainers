53118019e5386811871cfe4d8689317c3997571e
Alex Wandres <alex.r.wandres@gmail.com>
2020-08-26 11:05:54 -0400

    fixed script JS error

--- components/layout.js
+++ components/layout.js
@@ -152,7 +152,7 @@ export default function Layout({ children, home }) {
 
 
         <script type="application/ld+json">
-  {{
+  {{`
     "@context": "http://schema.org",
     "@type": "Organization",
     "name": "Vetted Trainers",
@@ -164,7 +164,7 @@ export default function Layout({ children, home }) {
       "https://www.instagram.com/vettedtrainers/",
       "https://www.youtube.com/channel/UCMTekJJglOmXvy3AZGjbQYw"
     ]
-  }}
+  `}}
 </script>
       </main>
       {!home && (
